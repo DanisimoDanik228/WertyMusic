4b53d40de732c53fdd7f1901f880d533f4797f7e
Werty <Werty@DESKTOP-LB170S7>
2025-11-22 18:26:11 +0300

    feat: add create and get music from local storage with basic compare music exist

---
+++ Domain/Models/Music.cs
@@ -0,0 +1,16 @@
+using Domain.Enum;
+
+namespace Domain.Models;
+
+public class Music
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string ArtistName { get; set; }
+    public string Url { get; set; }
+    public string DownloadUrl { get; set; }
+    public DateTime CreationDate  { get; set; }
+    public SiteSource SiteSource { get; set; }
+    
+    public string SourceName { get; set; }
+}
