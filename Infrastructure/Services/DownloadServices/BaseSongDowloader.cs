dd6c04884f1ffeedcd43d66174cea91526a46a39
Werty <Werty@DESKTOP-LB170S7>
2025-12-06 01:28:31 +0300

    feat: clear base downloader

--- Infrastructure/Services/DownloadServices/BaseSongDowloader.cs
+++ Infrastructure/Services/DownloadServices/BaseSongDowloader.cs
@@ -10,9 +10,8 @@ namespace Infrastructure.Services.DownloadServices;
 
 public abstract class BaseSongDowloader
 {
-    protected abstract string storageFolder { get; }
+    protected abstract string _storageFolder { get; }
     protected abstract int _maxCountSongForSearchSong { get; }
-    protected abstract string CreateUrlForSearch(string inputName);
 
     protected StorageOptions _storageOptions;
     public BaseSongDowloader(IOptions<StorageOptions> options)
@@ -21,32 +20,7 @@ public abstract class BaseSongDowloader
     }
 
     private static readonly char[] InvalidFileNameChars = Path.GetInvalidFileNameChars();
-    protected static IWebDriver SetupDriver()
-    {
-        ChromeDriverService service = ChromeDriverService.CreateDefaultService();
-        service.EnableVerboseLogging = false;
-        service.SuppressInitialDiagnosticInformation = true;
-        service.HideCommandPromptWindow = true;
-
-        ChromeOptions options = new ChromeOptions();
-
-        options.PageLoadStrategy = PageLoadStrategy.Normal;
-
-        options.AddArgument("--window-size=1920,1080");
-        options.AddArgument("--no-sandbox");
-        options.AddArgument("--headless");
-        options.AddArgument("--disable-gpu");
-        options.AddArgument("--disable-crash-reporter");
-        options.AddArgument("--disable-extensions");
-        options.AddArgument("--disable-in-process-stack-traces");
-        options.AddArgument("--disable-logging");
-        options.AddArgument("--disable-dev-shm-usage");
-        options.AddArgument("--log-level=3");
-        options.AddArgument("--output=/dev/null");
-        options.AddExcludedArgument("enable-logging");
-
-        return new ChromeDriver(options);
-    }
+  
     protected static string SanitizeFileName(string fileName, char replacementChar = ' ')
     {
         if (string.IsNullOrEmpty(fileName))
