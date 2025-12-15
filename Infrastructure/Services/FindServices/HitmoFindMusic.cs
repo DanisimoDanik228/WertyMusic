35f98f786d810334df41b98c3d3af70a8734aeb9
Werty <Werty@DESKTOP-LB170S7>
2025-12-15 21:39:08 +0300

    feat: add new Source Site

---
+++ Infrastructure/Services/FindServices/HitmoFindMusic.cs
@@ -0,0 +1,84 @@
+using Domain.Enum;
+using Domain.Interfaces.DownloadServices;
+using Domain.Models;
+using HtmlAgilityPack;
+using Infrastructure.Options;
+using Microsoft.Extensions.Options;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using PuppeteerSharp;
+
+namespace Infrastructure.Services.DownloadServices;
+
+public class HitmoFindMusic  : BaseMusicFind, IMusicFindService
+{
+    private IBrowser? _browser;
+    protected override int _maxCountSongForSearchSong { get; }
+    
+    
+    public HitmoFindMusic(IOptions<StorageOptions> options) : base(options)
+    {        
+        this._maxCountSongForSearchSong = 5;
+    }
+    private static string CreateUrlForSearch(string inputName)
+    {
+        return $"https://eu.hitmo-top.com/search?q={inputName.Replace(" ", "%20")}";
+    }
+
+    private static string FixSongName(string inputName)
+    {
+        int startIndex = 0;
+
+        while (startIndex < inputName.Length && (inputName[startIndex] == ' ' || inputName[startIndex] == '\r'|| inputName[startIndex] == '\n'))
+        {
+            startIndex++;
+        }
+        
+        int  endIndex = inputName.Length - 1;
+        while (endIndex > startIndex && (inputName[startIndex] == ' ' || inputName[startIndex] == '\r'|| inputName[startIndex] == '\n'))
+        {
+            endIndex--;
+        }
+        
+        return inputName.Substring(startIndex, endIndex - startIndex);
+    }
+
+    public async Task<IEnumerable<Music>> FindMusicsAsync(string musicName)
+    {
+        List<Music> res = new();
+        var chromeOptions = new ChromeOptions();
+        chromeOptions.AddArgument("--headless"); 
+        
+        using (IWebDriver driver = new ChromeDriver(chromeOptions))
+        {        
+            driver.Navigate().GoToUrl(CreateUrlForSearch(musicName));
+            
+            string htmlContent = driver.PageSource;
+            
+            var document = new HtmlDocument();
+            document.LoadHtml(htmlContent);
+            
+            var liElements = document.DocumentNode.SelectNodes("//ul[@class='tracks__list']/li");
+
+            for (int i = 0; i < liElements.Count && i < _maxCountSongForSearchSong; i++)
+            {
+                Music music = new();
+                    
+                var liElement  = liElements[i];
+                music.ArtistName = liElement.SelectSingleNode(".//div[@class='track__desc']")?.InnerText; 
+                var firstMusicName = liElement.SelectSingleNode(".//div[@class='track__title']")?.InnerText;
+                music.MusicName = FixSongName(firstMusicName);
+                
+                var musicLinks = liElement.SelectSingleNode(".//a[contains(@href, '/get/music/')]");
+                music.DownloadUrl = musicLinks.GetAttributeValue("href", "");
+                music.SiteSource = SiteSource.B;
+                music.SourceName = musicName;
+                music.CreationDate = DateTime.UtcNow;
+                
+                res.Add(music);
+            }
+        }
+        
+        return res;
+    }
+}
