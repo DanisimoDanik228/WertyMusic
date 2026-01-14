55f54e7b0c0fcdd45b679d073f200985acc2e9e4
danil <danilaorlovic28@gmail.com>
2026-01-14 18:28:00 +0300

    feat: add migration db

---
+++ Infrastructure/Migrations/20260112192143_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Musics",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MusicName = table.Column<string>(type: "text", nullable: false),
+                    ArtistName = table.Column<string>(type: "text", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: true),
+                    ArtistUrl = table.Column<string>(type: "text", nullable: true),
+                    DownloadUrl = table.Column<string>(type: "text", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SiteSource = table.Column<int>(type: "integer", nullable: false),
+                    SourceName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musics", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Musics");
+        }
+    }
+}
